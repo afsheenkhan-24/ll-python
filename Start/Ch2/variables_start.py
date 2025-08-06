26514da00c4717bee73bbaeee87cb9e530f5cb2d
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2025-08-06 11:35:56 +0100

    complete helloworld and variables

--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,28 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
-
+print(myint)
+print(myfloat)
+print(mystr)
+print(mybool)
 
 # Operators are used to perform operations on variables
+print(myint + myfloat)
+print(myint * myfloat)
+print(myint / myfloat)
+print(myint % 3)
 
+str1 = "This is text"
+print(mystr + str1)
 
 # Logical and comparison operators 
+print(myint == 10)
+print(myfloat != 20)
+print(myfloat > 20)
+print(myfloat < 10)
 
+print(not(myint > 5 and myfloat < 25))
 
 # re-declaring a variable works
+myint = "abc"
+print(myint)
